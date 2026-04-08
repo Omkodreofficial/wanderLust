404c0f83b3617bfb1ce77af31ce67bdba7119c7f
OmKodreofficial <omkodre10423@gmail.com>
2026-04-09 00:07:18 +0530

    feat: add review system, improve UI, fix routes, and implement cascade delete for reviews

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema
 
 
@@ -15,9 +16,22 @@ const listingSchema = new Schema({
     },
     price: Number,
     location: String,
-    country: String
+    country: String,
+    reviews:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        }
+    ]
 })
 
+
+listingSchema.post("findOneAndDelete", async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id: {$in: listing.reviews}})
+    }
+    
+})
 const Listing = mongoose.model("Listing", listingSchema)
 
 module.exports = Listing;
